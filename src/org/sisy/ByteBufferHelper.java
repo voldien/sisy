cdddf275cf7947e7063da46f7bd730052bdf2b3a
Voldie <voldiekami@gmail.com>
2017-10-30 21:04:43 +0100

    Minor comment added.

--- src/org/sisy/ByteBufferHelper.java
+++ src/org/sisy/ByteBufferHelper.java
@@ -6,20 +6,22 @@ import java.nio.ByteOrder;
 /**
  * Responsible of making all
  * byte buffer with the same byte ordering.
+ *
+ * @author Valdemar Lindberg
  */
 public class ByteBufferHelper {
 
     /**
      * Create little endian byte buffer.
      *
-     * @param capacity
+     * @param capacity size in number of bytes.
      * @return non-null ByteBuffer.
      * @throws IllegalArgumentException if the capacity is invalid.
      */
     public static ByteBuffer createByteBuffer(int capacity) throws Exception {
         if (capacity <= 0)
             throw new IllegalArgumentException("Capacity must be a non-negative size");
-
+        /*  Allocate buffer and set byte ordering.  */
         ByteBuffer byteBuffer = ByteBuffer.allocate(capacity);
         byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
         return byteBuffer;
