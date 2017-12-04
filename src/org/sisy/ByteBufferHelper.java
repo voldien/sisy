80ad67dd099eba788c7767bd0a027a3a39b83514
Voldie <voldiekami@gmail.com>
2017-12-04 08:48:52 +0100

    Minor.

--- src/org/sisy/ByteBufferHelper.java
+++ src/org/sisy/ByteBufferHelper.java
@@ -15,12 +15,13 @@ public class ByteBufferHelper {
      * Create little endian byte buffer.
      *
      * @param capacity size in number of bytes.
-     * @return non-null ByteBuffer.
+     * @return non-null ByteBuffer object.
      * @throws IllegalArgumentException if the capacity is invalid.
      */
     public static ByteBuffer createByteBuffer(int capacity) throws Exception {
         if (capacity <= 0)
             throw new IllegalArgumentException("Capacity must be a non-negative size");
+
         /*  Allocate buffer and set byte ordering.  */
         ByteBuffer byteBuffer = ByteBuffer.allocate(capacity);
         byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
